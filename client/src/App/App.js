6e0c514a276672d2a22cca66bebb7af11ace3b35
Inna <kurylo.salamonova@gmail.com>
2023-03-09 14:50:31 +0200

    first commit

---
+++ client/src/App/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
+import RootRouters from "../Router/Router";
+import theme from "../theming";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import { setModal } from "../store/reducers/modalSlice";
+// eslint-disable-next-line import/named
+import { modals } from "../components/Modal/configs";
+
+const App = () => {
+	const modal = useSelector((state) => state.modal.value);
+	const dispatch = useDispatch();
+	const actionModalHandler = (status) => {
+		dispatch(setModal(status));
+	};
+
+	const activeModal = modals[modal] ?? null;
+	return (
+		<ThemeProvider theme={theme}>
+			<BrowserRouter>
+				{activeModal}
+				<Header modal={actionModalHandler} />
+				<RootRouters />
+				<Footer />
+			</BrowserRouter>
+		</ThemeProvider>
+	);
+};
+export default App;
---
+++ client/src/components/PopularProducts/PopularProducts.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { Button, Container, Box } from "@mui/material";
+import CategoryTitle from "../CategoryTitle";
+import ProductCard from "../ProductCard";
+
+const PopularProducts = ({ products, advertisement = false }) => {
+	return (
+		<Container>
+			<Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+				{!advertisement && <CategoryTitle text="Популярні товари" />}
+				{advertisement && <CategoryTitle text="Ви також можете розглянути товари на знижкі" />}
+				{!advertisement && (
+					<Link style={{ textDecoration: "none" }} to="/products">
+						<Button color="secondary" variant="contained">
+							Усі товари
+						</Button>
+					</Link>
+				)}
+			</Box>
+			<CardsContainer>
+				{products?.map((card, index) => {
+					if (!advertisement) {
+						return card.popular && <ProductCard key={index} card={card} withCart={false} />;
+					}
+					return card.sale && <ProductCard key={index} card={card} withCart={false} />;
+				})}
+			</CardsContainer>
+		</Container>
+	);
+};
+export const CardsContainer = styled.div`
+	display: flex;
+	gap: 50px;
+	max-height: 700px;
+	padding: 35px;
+	overflow: scroll;
+`;
+export default PopularProducts;
