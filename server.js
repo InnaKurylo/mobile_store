a754e455237f46ed068c333990e0b3750daa13db
Inna <kurylo.salamonova@gmail.com>
2023-03-11 22:23:09 +0200

    https://fe-04-2.onrender.com/api

--- server.js
+++ server.js
@@ -27,6 +27,11 @@ const partners = require("./routes/partners");
 // const mainRoute = require('./routes/index');
 
 const app = express();
+app.use(express.json());
+const corsOption = {
+  origin: ["http://localhost:3000"],
+};
+app.use(cors(corsOption));
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
