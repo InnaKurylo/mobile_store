2dfc55e2b4a886c76c6fc051eeb443a52d3c6314
Inna <kurylo.salamonova@gmail.com>
2023-03-12 00:06:16 +0200

    https://fe-04-2.onrender.com:/10000

--- client/src/config/API.js
+++ client/src/config/API.js
@@ -1,2 +1,2 @@
-export const DOMAIN = "http://localhost:10000/api";
-export const DOMAIN2 = "http://localhost:5000/";
+export const DOMAIN = "https://fe-04-2.onrender.com:10000/api";
+export const DOMAIN2 = "https://fe-04-2.onrender.com:10000/";
