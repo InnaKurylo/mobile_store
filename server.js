ff94371ffd05577de63f43f60aa6523357a3bc2a
Inna <kurylo.salamonova@gmail.com>
2023-03-11 23:05:07 +0200

    log port 5000

--- server.js
+++ server.js
@@ -83,6 +83,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = 5000;
-
+const port = process.env.PORT || 5000;
+console.log("port", port);
 app.listen(port, () => console.log(`Server running on port ${port}`));
